4b06a241fd8e4abe08392102183bbe0f82c45b03
tomhockenberry <tomhockenberry03@gmail.com>
2021-03-29 08:51:01 -0400

    new file

--- helloworld.c
+++ helloworld.c
@@ -1,4 +1,13 @@
+/*#include <iostream>
+using namespace std;
+
 int main() {
     cout << "Hello World!";
+    return 0;
+}*/
+#include <stdio.h>
+
+int main(){
+    puts("Hello World");
     return 0;
 }
